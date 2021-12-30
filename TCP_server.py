be652ec6356e8e9d7d0f36827035cabe7c8e9726
101bingo <xu_peter@163.com>
2021-12-30 20:05:56 +0800

    tcp服务文件

---
+++ TCP_server.py
@@ -0,0 +1,24 @@
+from socket import AF_INET, SOCK_STREAM, socket
+
+ip_address = '42.193.138.254'
+server_port = 6222
+
+tcp_server = socket(AF_INET, SOCK_STREAM)
+
+tcp_server.bind((ip_address, server_port))
+
+tcp_server.listen(3)
+
+try:
+    while True:
+        conn, client_addr = tcp_server.accept()
+        try:
+            while True:
+                recv = conn.recv(1024)
+                if recv:
+                    print('客户端{0}:{1}'.format(client_addr, recv))
+        finally:
+            conn.close()
+finally:
+    tcp_server.close()
+
