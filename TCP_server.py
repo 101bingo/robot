020a11faac253edd76c58326d6f5886007e867fc
101bingo <xu_peter@163.com>
2022-06-02 00:51:01 +0800

    test

--- TCP_server.py
+++ TCP_server.py
@@ -56,13 +56,13 @@ def dispose_client_request(tcp_client, client_address):
         recv_data = tcp_client.recv(256)
 
         ws.ping()
-        # ws_res = ws.recv_data_frame()
-        ws_res = ws.recv_frame()
+        ws_res = ws.recv_data_frame(control_frame=False)
+        # ws_res = ws.recv_frame()
         logger.debug(f'recv_fram:{ws_res}')
-        if ws_res[0] == 9:
-            ws.pong()
-        elif not ws_res.opcode == 10:
-            ws.connect('ws://127.0.0.1:8002/livedata')
+        # if ws_res == 9:
+        #     ws.pong()
+        # elif not ws_res.opcode == 10:
+        #     ws.connect('ws://127.0.0.1:8002/livedata')
         #有消息处理
         if recv_data:
             res = [int(i) for i in recv_data]
