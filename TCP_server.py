9861aa0b1dbb3152bb2e80d8b4f03c929833574c
101bingo <xu_peter@163.com>
2022-06-01 23:58:58 +0800

    test

--- TCP_server.py
+++ TCP_server.py
@@ -47,7 +47,7 @@ def send_live_data_to_background(oxygen, temperature):
 
 def dispose_client_request(tcp_client, client_address):
     start_time = datetime.now()
-    ws = websocket.WebSocket().connect('ws://127.0.0.1:8082/livedata')
+    ws = websocket.WebSocket().connect('ws://127.0.0.1:8002/livedata')
     #循环接受或发送数据
     while True:
         run_time = datetime.now()
