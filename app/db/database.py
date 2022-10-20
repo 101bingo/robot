6ad5226a8ed95a640f7f4b4bc82e9b1c604ffa23
101bingo <xu_peter@163.com>
2022-10-20 10:32:50 +0800

    修复数据库连接断开问题

--- app/db/database.py
+++ app/db/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import AsyncSession,create_async_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 
 MYSQL_URL = "mysql+aiomysql://root:Xu123456@127.0.0.1:3306/fishdata"
 # MYSQL_URL = "mysql+aiomysql://root@127.0.0.1:3306/fishdata"
@@ -9,7 +10,7 @@ MYSQL_URL = "mysql+aiomysql://root:Xu123456@127.0.0.1:3306/fishdata"
 # engine = create_engine(MYSQL_URL, encoding='utf-8', echo=True)
 # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 """ 数据库异步操作 """
-engine = create_async_engine(MYSQL_URL, encoding='utf-8', echo=True)
+engine = create_async_engine(MYSQL_URL, encoding='utf-8', echo=True, poolclass=NullPool)
 
 async_SessionLocal = sessionmaker(
     class_=AsyncSession,
